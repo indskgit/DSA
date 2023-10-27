67feadedc7d622fce67aeea002fdba6597c16e23
Saurabh Kumar <skumarsaurabh2001@gmail.com>
2023-10-28 00:23:24 +0530

    Solved 1 leetCode Ques

--- src/GFG/POTD/MaxSumIncreasingSubsequence.java
+++ src/GFG/POTD/MaxSumIncreasingSubsequence.java
@@ -1,10 +1,10 @@
 package GFG.POTD;
 
 public class MaxSumIncreasingSubsequence {
-    static void maxSum(int arr[], int n)
-    {
-
-    }
+//    static int maxSum(int arr[], int n)
+//    {
+//
+//    }
     public static void main(String[] args) {
         int[] arr = {1,101,2,3,100};
         int[] arr1 = {9,4,8,2};
